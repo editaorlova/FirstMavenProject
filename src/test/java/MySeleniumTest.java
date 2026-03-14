350f62d37b3d449e1d753faee4b128c29d334da8
edita.orlova <edita.orlova@solva.kz>
2026-03-15 00:22:46 +0100

    Selenium

---
+++ src/test/java/MySeleniumTest.java
@@ -0,0 +1,66 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class MySeleniumTest {
+
+    @Test
+    public void testFirst(){
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        Assert.assertEquals(message.getText(), "Received!");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testGoogle() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+        WebElement textInput = driver.findElement(By.id("#APjFqb"));
+        textInput.sendKeys("selenium");
+
+        WebElement aiButton = driver.findElement(By.xpath("//button[@jsname='B6rgad']"));
+        aiButton.click();
+
+        WebElement text = driver.findElement(By.xpath("//div[@jsuid='r629cf_7t']"));
+        Assert.assertEquals(text.getText(),"Основные компоненты");
+
+        driver.quit();
+
+
+    }
+
+    @Test
+    public void test() {
+    WebDriver driver = new ChromeDriver();
+
+    driver.get("https://www.wikipedia.org/");
+    driver.findElement(By.id("searchInput")).sendKeys("Selenium");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+    WebElement text = driver.findElement(By.xpath("//h1/span"));
+
+    Assert.assertEquals(text.getText(),"Selenium");
+
+    driver.quit();
+    }
+}
